297d2a1ca60b57eb2da9f1ea3b676efb25478e18
Gus Humphrey <ghumphrey@captechconsulting.com>
2019-01-07 12:48:25 -0500

    Initial Commit

---
+++ packages/grocery/index.js
@@ -0,0 +1,7 @@
+const sillyname = require('sillyname');
+const apple = require('apple');
+const banana = require('banana');
+
+console.log(`Grocery and ${sillyname()}`);
+console.log(apple);
+console.log(banana);
